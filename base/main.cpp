65060db41c6865d04b9a278591cd4cee1c3e4e8c
Heshan Padmasiri <heshanp@wso2.com>
2022-07-31 08:34:47 +0530

    Initial commit

---
+++ base/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int	main()
+{
+    int count;
+    scanf("%d", &count);
+    while (count--)
+    {
+        int a, b; 
+        scanf("%d %d", &a, &b);
+        int result = a + b;
+        printf("%d \n", result);
+    }
+    
+    return 0;
+}
