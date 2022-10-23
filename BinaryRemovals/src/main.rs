a32a8986a1e198e6f2c0ae942725df165190369c
Heshan Padmasiri <hpheshan@gmail.com>
2022-10-23 12:12:55 +0530

    none dp version

--- BinaryRemovals/src/main.rs
+++ BinaryRemovals/src/main.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::collections::HashMap;
 
 fn main() {
     let mut t = String::new();
@@ -20,78 +19,41 @@ fn solve() -> bool {
     let mut line = String::new();
     io::stdin().read_line(&mut line).expect("expect input line");
     let chars = line.trim().chars().collect::<Vec<char>>();
-    let mut dp = HashMap::new();
-    return solve_inner(&chars, 0, &init_skip_indicies(), &mut dp);
-}
-
-fn solve_inner(vals: &Vec<char>, index: usize, current: &SkipIndicies, dp: &mut HashMap<u128, bool>) -> bool {
-    // println!("index : {index} current: {:?}", current.indicies);
-    if is_sorted(vals, current, dp) {
-        // println!("\tsorted");
+    // TODO: special case less than 2
+    if chars.len() <= 2 {
         return true;
     }
-    if index >= vals.len() {
-        // println!("\tend");
-        return false;
+    return solve_inner(&chars, 0, None, None);
+}
+
+fn solve_inner(chars: &Vec<char>, index: usize, last_val: Option<char>, last_skip_index: Option<usize>) -> bool {
+    // println!("index : {}, last_val: {:?}, last_skip_index: {:?}", index, last_val, last_skip_index);
+    if index >= chars.len() {
+        return true;
     }
-    let is_skippable;
-    if index == 0 || current.size == 0 {
-        is_skippable = true;
+    let mut must_skip = false;
+    if last_val.is_some() {
+        let top = last_val.unwrap();
+        if top > chars[index] {
+            must_skip = true;
+        }
+    }
+    // lhs is sorted
+    if !must_skip {
+        let inclusive = solve_inner(chars, index + 1, Some(chars[index]), last_skip_index);
+        if inclusive {
+            return true;
+        }
+    }
+    let skippable;
+    if last_skip_index.is_some() {
+        skippable = last_skip_index.unwrap() + 1 < index; 
     }
     else {
-        is_skippable = index > current.indicies[current.size - 1] + 1;
+        skippable = true;
     }
-    // println!("\tskippable: {is_skippable}");
-    return solve_inner(vals, index + 1, current, dp) || (is_skippable && solve_inner(vals, index + 1, &add_skip_index(current, index), dp));
-}
-
-fn is_sorted(vals: &Vec<char>, skip_indices: &SkipIndicies, dp: &mut HashMap<u128, bool>) -> bool {
-    let memo = dp.get(&skip_indices.hash);
-    if memo.is_some() {
-        return *memo.unwrap();
+    if skippable {
+        return solve_inner(chars, index + 1, last_val, Some(index)); 
     }
-    let new_vals = filtere_vals(vals, skip_indices);
-    for i in 1 .. new_vals.len() {
-        if new_vals[i-1] > new_vals[i] {
-            dp.insert(skip_indices.hash, false);
-            return false;
-        }
-    }
-    dp.insert(skip_indices.hash, true);
-    return true;
-}
-
-struct SkipIndicies {
-    indicies: Vec<usize>,
-    size: usize,
-    hash: u128
-}
-
-fn init_skip_indicies() -> SkipIndicies {
-    return SkipIndicies { indicies: vec![], size: 0, hash: 0 };
-}
-
-fn add_skip_index(current: &SkipIndicies, new_index: usize) -> SkipIndicies {
-    let mut indicies = current.indicies.to_vec();
-    let hash = current.hash | (1 << new_index);
-    indicies.push(new_index);
-    return SkipIndicies { indicies, size: current.size + 1, hash };
-}
-
-fn filtere_vals(vals: &Vec<char>, skip_indices: &SkipIndicies) -> Vec<char> {
-    let mut new_vals = vec![];
-    new_vals.reserve(vals.len() - skip_indices.indicies.len());
-    let mut skip_index = 0;
-    for i in 0 .. vals.len() {
-        if skip_index >= skip_indices.indicies.len() {
-            new_vals.push(vals[i]);
-        }
-        else if skip_indices.indicies[skip_index] == i {
-            skip_index += 1;
-        }
-        else {
-            new_vals.push(vals[i]);
-        }
-    }
-    return new_vals;
+    return false;
 }
