bbbe06defbe6dd630ac058bacc486d622bb58ba7
Heshan Padmasiri <hpheshan@gmail.com>
2022-10-30 09:01:23 +0530

    clean up code in base

--- rust_base/src/main.rs
+++ rust_base/src/main.rs
@@ -1,15 +1,25 @@
 use std::io;
 
 fn main() {
-    let mut n = String::new();
-    io::stdin().read_line(&mut n).expect("Failed to read n");
-    let _n: i64 = n.trim().parse().expect("Expect n to be integer");
-    
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).expect("expect input line");
-    let nums = line.trim().split(' ').flat_map(str::parse::<i64>).collect::<Vec<_>>();
-
+    let n = read_int();
+    let nums = read_array();
     let sum: i64 = nums.iter().sum();
 
     println!("{}", sum);
+}
+
+fn read_int() -> i64 {
+    let line = read_line();
+    return line.trim().parse().expect("expect an integer");
+}
+
+fn read_array() -> Vec<i64> {
+    let line = read_line();
+    return line.trim().split(' ').flat_map(str::parse::<i64>).collect::<Vec<_>>();
+}
+
+fn read_line() -> String {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("expect input line");
+    return line;
 }
