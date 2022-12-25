bdc84e2a46537a72f6d5dc5d8d67c842b4c52adf
Heshan Padmasiri <hpheshan@gmail.com>
2022-12-25 09:56:14 +0530

    factor input to trait object

--- rust_base/src/main.rs
+++ rust_base/src/main.rs
@@ -1,25 +1,31 @@
 use std::io;
 
 fn main() {
-    let n = read_int();
-    let nums = read_array();
+    let mut reader = std_reader();
+    let reader_ref = &mut reader;
+    let n = read_int(reader_ref);
+    let nums = read_array(reader_ref);
     let sum: i64 = nums.iter().sum();
 
     println!("{}", sum);
 }
 
-fn read_int() -> i64 {
-    let line = read_line();
+fn read_int(reader: &mut Box<dyn io::BufRead>) -> i64 {
+    let line = read_line(reader);
     return line.trim().parse().expect("expect an integer");
 }
 
-fn read_array() -> Vec<i64> {
-    let line = read_line();
+fn read_array(reader: &mut Box<dyn io::BufRead>) -> Vec<i64> {
+    let line = read_line(reader);
     return line.trim().split(' ').flat_map(str::parse::<i64>).collect::<Vec<_>>();
 }
 
-fn read_line() -> String {
+fn std_reader() -> Box<dyn io::BufRead> {
+    Box::new(io::BufReader::new(io::stdin()))
+}
+
+fn read_line(input_reader: &mut Box<dyn io::BufRead>) -> String {
     let mut line = String::new();
-    io::stdin().read_line(&mut line).expect("expect input line");
-    return line;
+    input_reader.read_line(&mut line).expect("expect input line");
+    line
 }
