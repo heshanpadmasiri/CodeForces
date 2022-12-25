e6e95a23a1b60f5e273009971b816bfe5ae4ecbb
Heshan Padmasiri <hpheshan@gmail.com>
2022-12-25 10:51:05 +0530

    Use cargo for testing (remove Make)

--- rust_base/src/main.rs
+++ rust_base/src/main.rs
@@ -3,11 +3,19 @@ use std::io;
 fn main() {
     let mut reader = std_reader();
     let reader_ref = &mut reader;
-    let n = read_int(reader_ref);
-    let nums = read_array(reader_ref);
+    for line in solve(reader_ref) {
+        println!("{}", line);
+    }
+}
+
+fn solve(input: &mut Box<dyn io::BufRead>) -> Vec<String> {
+    let _n = read_int(input);
+    let nums = read_array(input);
     let sum: i64 = nums.iter().sum();
 
-    println!("{}", sum);
+    let output = vec![ sum.to_string()];
+
+    output
 }
 
 fn read_int(reader: &mut Box<dyn io::BufRead>) -> i64 {
@@ -28,4 +36,62 @@ fn read_line(input_reader: &mut Box<dyn io::BufRead>) -> String {
     let mut line = String::new();
     input_reader.read_line(&mut line).expect("expect input line");
     line
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use std::io::{self, BufRead, BufReader};
+    use std::path::Path;
+
+    use crate::solve;
+
+    #[derive(Debug)]
+    struct TestCase {
+        input_file: std::path::PathBuf,
+        output_file: std::path::PathBuf,
+    }
+
+    #[test]
+    fn test() {
+        for each in get_test_cases() {
+            println!("TEST_CASE: {:?}", each);
+
+            let mut reader = test_case_reader(&each);
+            let actual = solve(&mut reader);
+            assert_eq!(actual, test_case_output(&each));
+        }
+    }
+
+    fn test_case_reader(test_case: &TestCase) -> Box<dyn BufRead> {
+        Box::new(io::BufReader::new(
+            fs::File::open(&test_case.input_file).unwrap(),
+        ))
+    }
+
+    fn test_case_output(test_case: &TestCase) -> Vec<String> {
+        let file = fs::File::open(&test_case.output_file).unwrap();
+        let buf_reader = BufReader::new(file);
+        buf_reader
+            .lines()
+            .map(|line| line.expect("failed to read line"))
+            .collect()
+    }
+
+    fn get_test_cases() -> Vec<TestCase> {
+        println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
+        let mut test_cases = Vec::new();
+        for each in fs::read_dir("./input").unwrap() {
+            let input_file = each.unwrap().path();
+            let output_file_name = input_file.file_stem().unwrap().to_str().unwrap();
+            let output_file =
+                Path::new(&format!("./output/{}.out", output_file_name)).to_path_buf();
+            assert!(output_file.exists());
+            test_cases.push(TestCase {
+                input_file,
+                output_file,
+            });
+        }
+        test_cases
+    }
 }
