ea95dadab0967d0382809c8acc333d29193e2b0c
Heshan Padmasiri <hpheshan@gmail.com>
2023-01-22 09:46:48 +0530

    PashmakAndFlowers solved

---
+++ PashmakAndFlowers/src/main.rs
@@ -0,0 +1,128 @@
+use std::{collections::HashMap, io};
+
+type Reader = Box<dyn io::BufRead>;
+type Int = i64;
+
+fn main() {
+    let mut reader = std_reader();
+    let reader_ref = &mut reader;
+    for line in solve(reader_ref) {
+        println!("{}", line);
+    }
+}
+
+fn solve(input: &mut Reader) -> Vec<String> {
+    let n = read_int(input);
+    let nums = read_array(input);
+    let mut counts = HashMap::<Int, Int>::new();
+    let mut min = Int::MAX;
+    let mut max = Int::MIN;
+    for each in nums {
+        match counts.get(&each) {
+            Some(count) => {
+                counts.insert(each, count + 1);
+            }
+            None => {
+                counts.insert(each, 1);
+            }
+        }
+        if each < min {
+            min = each;
+        }
+        if each > max {
+            max = each;
+        }
+    }
+    let max_diff = max - min;
+    let n_ways = if max != min {
+        counts.get(&min).expect("expect min to exists")
+            * counts.get(&max).expect("expect max to exist")
+    } else {
+        (n * (n - 1)) / 2
+    };
+    [format!("{} {}", max_diff, n_ways)].to_vec()
+}
+
+fn read_int(reader: &mut Reader) -> Int {
+    let line = read_line(reader);
+    return line.trim().parse().expect("expect an integer");
+}
+
+fn read_array(reader: &mut Reader) -> Vec<Int> {
+    let line = read_line(reader);
+    return line
+        .trim()
+        .split(' ')
+        .flat_map(str::parse::<Int>)
+        .collect::<Vec<_>>();
+}
+
+fn std_reader() -> Reader {
+    Box::new(io::BufReader::new(io::stdin()))
+}
+
+fn read_line(input_reader: &mut Reader) -> String {
+    let mut line = String::new();
+    input_reader
+        .read_line(&mut line)
+        .expect("expect input line");
+    line
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use std::io::{self, BufRead, BufReader};
+    use std::path::Path;
+
+    use crate::solve;
+
+    #[derive(Debug)]
+    struct TestCase {
+        input_file: std::path::PathBuf,
+        output_file: std::path::PathBuf,
+    }
+
+    #[test]
+    fn test() {
+        for each in get_test_cases() {
+            println!("TEST_CASE: {:?}", each);
+
+            let mut reader = test_case_reader(&each);
+            let actual = solve(&mut reader);
+            assert_eq!(actual, test_case_output(&each));
+        }
+    }
+
+    fn test_case_reader(test_case: &TestCase) -> Box<dyn BufRead> {
+        Box::new(io::BufReader::new(
+            fs::File::open(&test_case.input_file).unwrap(),
+        ))
+    }
+
+    fn test_case_output(test_case: &TestCase) -> Vec<String> {
+        let file = fs::File::open(&test_case.output_file).unwrap();
+        let buf_reader = BufReader::new(file);
+        buf_reader
+            .lines()
+            .map(|line| line.expect("failed to read line"))
+            .collect()
+    }
+
+    fn get_test_cases() -> Vec<TestCase> {
+        println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
+        let mut test_cases = Vec::new();
+        for each in fs::read_dir("./input").unwrap() {
+            let input_file = each.unwrap().path();
+            let output_file_name = input_file.file_stem().unwrap().to_str().unwrap();
+            let output_file =
+                Path::new(&format!("./output/{}.out", output_file_name)).to_path_buf();
+            assert!(output_file.exists());
+            test_cases.push(TestCase {
+                input_file,
+                output_file,
+            });
+        }
+        test_cases
+    }
+}
