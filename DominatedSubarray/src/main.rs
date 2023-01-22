a1536522125d51044e510c8af03bb8dc0efabc36
Heshan Padmasiri <hpheshan@gmail.com>
2023-01-22 09:04:13 +0530

    Dominated subarray solved

---
+++ DominatedSubarray/src/main.rs
@@ -0,0 +1,115 @@
+use std::{io, collections::HashMap};
+
+type Reader = Box<dyn io::BufRead>;
+type Int = i64;
+
+fn main() {
+    let mut reader = std_reader();
+    let reader_ref = &mut reader;
+    for line in solve(reader_ref) {
+        println!("{}", line);
+    }
+}
+
+fn solve(reader: &mut Reader) -> Vec<String> {
+    let t = read_int(reader);
+    (0..t).map(|_| {
+        let n = read_int(reader) as usize;
+        let array = read_array(reader);
+        dominated_subarray_len(n, &array).to_string()
+    }).collect()
+}
+
+fn dominated_subarray_len(n: usize, array: &[Int]) -> Int {
+    let mut last_occurance = HashMap::<Int, usize>::new();
+    let mut shortest = usize::MAX;
+    for i in 0..n {
+        let val = array[i];
+        if let Some(last_index) = last_occurance.get(&val) {
+                let len = (i - last_index) + 1;
+                if len < shortest {
+                    shortest = len;
+                }
+        }
+        last_occurance.insert(val, i);
+    }
+    if shortest == usize::MAX { -1 } else { shortest as Int }
+}
+
+fn read_int(reader: &mut Reader) -> Int {
+    let line = read_line(reader);
+    return line.trim().parse().expect("expect an integer");
+}
+
+fn read_array(reader: &mut Reader) -> Vec<Int> {
+    let line = read_line(reader);
+    return line.trim().split(' ').flat_map(str::parse::<Int>).collect::<Vec<_>>();
+}
+
+fn std_reader() -> Reader {
+    Box::new(io::BufReader::new(io::stdin()))
+}
+
+fn read_line(input_reader: &mut Reader) -> String {
+    let mut line = String::new();
+    input_reader.read_line(&mut line).expect("expect input line");
+    line
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use std::io::{self, BufRead, BufReader};
+    use std::path::Path;
+
+    use crate::solve;
+
+    #[derive(Debug)]
+    struct TestCase {
+        input_file: std::path::PathBuf,
+        output_file: std::path::PathBuf,
+    }
+
+    #[test]
+    fn test() {
+        for each in get_test_cases() {
+            println!("TEST_CASE: {:?}", each);
+
+            let mut reader = test_case_reader(&each);
+            let actual = solve(&mut reader);
+            assert_eq!(actual, test_case_output(&each));
+        }
+    }
+
+    fn test_case_reader(test_case: &TestCase) -> Box<dyn BufRead> {
+        Box::new(io::BufReader::new(
+            fs::File::open(&test_case.input_file).unwrap(),
+        ))
+    }
+
+    fn test_case_output(test_case: &TestCase) -> Vec<String> {
+        let file = fs::File::open(&test_case.output_file).unwrap();
+        let buf_reader = BufReader::new(file);
+        buf_reader
+            .lines()
+            .map(|line| line.expect("failed to read line"))
+            .collect()
+    }
+
+    fn get_test_cases() -> Vec<TestCase> {
+        println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
+        let mut test_cases = Vec::new();
+        for each in fs::read_dir("./input").unwrap() {
+            let input_file = each.unwrap().path();
+            let output_file_name = input_file.file_stem().unwrap().to_str().unwrap();
+            let output_file =
+                Path::new(&format!("./output/{}.out", output_file_name)).to_path_buf();
+            assert!(output_file.exists());
+            test_cases.push(TestCase {
+                input_file,
+                output_file,
+            });
+        }
+        test_cases
+    }
+}
