d027a4724ab70ab218cb0c8644e8658d67135e5d
Heshan Padmasiri <hpheshan@gmail.com>
2023-01-29 09:44:32 +0530

    Polygon solved

---
+++ Polygon/src/main.rs
@@ -0,0 +1,140 @@
+use std::io;
+
+type Reader = Box<dyn io::BufRead>;
+type Int = usize;
+
+fn main() {
+    let mut reader = std_reader();
+    let reader_ref = &mut reader;
+    for line in solve(reader_ref) {
+        println!("{}", line);
+    }
+}
+
+fn solve(input: &mut Reader) -> Vec<String> {
+    let t = read_int(input);
+    (0..t).map(|_| {solve_polygon(input)}).collect()
+}
+
+fn solve_polygon(input: &mut Reader) -> String {
+    let n = read_int(input);
+    let mut polygon = read_polygon(input, n);
+    // mark border hits as valid
+    for i in 0..n {
+        if polygon[n-1][i] == State::PossibleHit {
+            polygon[n-1][i] = State::Hit;
+        }
+        if polygon[i][n-1] == State::PossibleHit {
+            polygon[i][n-1] = State::Hit;
+        }
+    }
+    for x in 0..n-1 {
+        for y in 0..n-1 {
+            let i = (n-2)-x;
+            let j = (n-2)-y;
+            if polygon[i][j] == State::PossibleHit {
+                if polygon[i][j+1] == State::Hit || polygon[i+1][j] == State::Hit {
+                    polygon[i][j] = State::Hit;
+                }
+                else {
+                    return "NO".to_owned();
+                }
+            }
+        }
+    }
+    "YES".to_owned()
+}
+
+#[derive(PartialEq, Eq, Debug)]
+enum State {
+    PossibleHit,
+    Hit,
+    Empty
+}
+
+type Polygon = Vec<Vec<State>>;
+
+fn read_polygon(input: &mut Reader, n: Int) -> Polygon {
+    (0..n).map(|_| { parse_line(read_line(input)) }).collect()
+}
+
+fn parse_line(line: String) -> Vec<State> {
+    line.trim().chars().map(|c| match c {
+       '0' => { State::Empty },
+       '1' => { State::PossibleHit },
+       _ => { panic!("unexpected char: {}", c) }
+    }).collect()
+}
+
+fn read_int(reader: &mut Reader) -> Int {
+    let line = read_line(reader);
+    return line.trim().parse().expect("expect an integer");
+}
+
+fn std_reader() -> Reader {
+    Box::new(io::BufReader::new(io::stdin()))
+}
+
+fn read_line(input_reader: &mut Reader) -> String {
+    let mut line = String::new();
+    input_reader.read_line(&mut line).expect("expect input line");
+    line
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use std::io::{self, BufRead, BufReader};
+    use std::path::Path;
+
+    use crate::solve;
+
+    #[derive(Debug)]
+    struct TestCase {
+        input_file: std::path::PathBuf,
+        output_file: std::path::PathBuf,
+    }
+
+    #[test]
+    fn test() {
+        for each in get_test_cases() {
+            println!("TEST_CASE: {:?}", each);
+
+            let mut reader = test_case_reader(&each);
+            let actual = solve(&mut reader);
+            assert_eq!(actual, test_case_output(&each));
+        }
+    }
+
+    fn test_case_reader(test_case: &TestCase) -> Box<dyn BufRead> {
+        Box::new(io::BufReader::new(
+            fs::File::open(&test_case.input_file).unwrap(),
+        ))
+    }
+
+    fn test_case_output(test_case: &TestCase) -> Vec<String> {
+        let file = fs::File::open(&test_case.output_file).unwrap();
+        let buf_reader = BufReader::new(file);
+        buf_reader
+            .lines()
+            .map(|line| line.expect("failed to read line"))
+            .collect()
+    }
+
+    fn get_test_cases() -> Vec<TestCase> {
+        println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
+        let mut test_cases = Vec::new();
+        for each in fs::read_dir("./input").unwrap() {
+            let input_file = each.unwrap().path();
+            let output_file_name = input_file.file_stem().unwrap().to_str().unwrap();
+            let output_file =
+                Path::new(&format!("./output/{}.out", output_file_name)).to_path_buf();
+            assert!(output_file.exists());
+            test_cases.push(TestCase {
+                input_file,
+                output_file,
+            });
+        }
+        test_cases
+    }
+}
